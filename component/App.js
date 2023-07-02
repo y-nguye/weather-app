e58987a150e0dbbd9a61f8b718e479fc599bedde
Y <nguyenhoaiy7@gmail.com>
2023-07-02 16:00:23 +0700

    Fix bug

--- component/App.js
+++ component/App.js
@@ -1,8 +1,8 @@
 import html from "../lib/core.js";
 import { connect } from "../lib/store.js";
 
-function App({ location }) {
-  console.log(location.map((x) => x.result));
+function App({ locations }) {
+  console.log();
   return html`
     <div id="search">
       <input
@@ -10,35 +10,40 @@ function App({ location }) {
         type="text"
         placeholder="Enter city name"
         spellcheck="false"
-        onkeyup="event.key === 'Enter' && dispatch('addLocation', this.value)"
+        onkeyup="event.key === 'Enter' && dispatch('addLocations', this.value)"
       />
       <button
-        onclick="dispatch('addLocation', document.getElementById('inputSearch').value)"
+        onclick="dispatch('addLocations', document.getElementById('inputSearch').value)"
       >
         <i class="fa-solid fa-magnifying-glass"></i>
       </button>
     </div>
     <div class="weather-container">
-      <h1 class="city">${location.map((x) => x.result.name).join(" ")}</h1>
+      <h1 class="city">${locations.map((x) => x.result.name).join("")}</h1>
       <h3 class="description">
-        ${location
-          .map((x) => x.result.weather.map((y) => y.description))
+        ${locations
+          .map((x) => x.result.weather?.map((y) => y.description))
           .join(" ")}
       </h3>
       <div class="weather">
         <h1 class="temp">
-          ${location.map((x) => Math.round(x.result.main.temp))}
-          <span class="unit-c">°</span>
+          ${locations.map((x) => Math.round(x.result.main?.temp))}
+          <span class="unit-c"
+            >${locations.map((x) => x.result.main?.temp).join("") && "°"}</span
+          >
         </h1>
         <img
           class="weather-icon"
-          src="./icons/${location
-            .map((x) => x.result.weather.map((y) => y.icon))
+          src="./icons/${locations
+            .map((x) => x.result.weather?.map((y) => y.icon))
             .join(" ")
-            .slice(0, -1)}.png"
+            .slice(0, -1) || "R"}.png"
         />
       </div>
-      <div class="details">
+      <div
+        class="details ${locations.map((x) => x.city).join("") ===
+          "City not found" && "hidden"}"
+      >
         <div class="col">
           <div class="details_container">
             <h4 class="title">
@@ -47,7 +52,7 @@ function App({ location }) {
             </h4>
             <div class="info">
               <p class="humidity">
-                ${location.map((x) => x.result.main.humidity)}%
+                ${locations.map((x) => x.result.main?.humidity)}%
               </p>
               <p class="message">The dew point right now</p>
             </div>
@@ -63,7 +68,7 @@ function App({ location }) {
 
             <div class="info">
               <p class="wind">
-                ${location.map((x) => x.result.wind.speed)} m/s
+                ${locations.map((x) => x.result.wind?.speed)} m/s
               </p>
               <p class="message">Current wind speed</p>
             </div>
@@ -77,7 +82,9 @@ function App({ location }) {
               Cloudiness
             </h4>
             <div class="info">
-              <p class="cloud">${location.map((x) => x.result.clouds.all)}%</p>
+              <p class="cloud">
+                ${locations.map((x) => x.result.clouds?.all)}%
+              </p>
               <p class="message">Cloud cover in the sky</p>
             </div>
           </div>
@@ -91,7 +98,7 @@ function App({ location }) {
             </h4>
             <div class="info">
               <p class="visibility">
-                ${location.map((x) => x.result.visibility) * 0.001} km
+                ${locations.map((x) => x.result?.visibility) * 0.001} km
               </p>
               <p class="message">It's perfectly clear right now</p>
             </div>
