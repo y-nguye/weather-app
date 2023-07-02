881dbd9f886470df4d28af4eaca0b7c2bdb7dd58
Y <nguyenhoaiy7@gmail.com>
2023-07-02 12:05:09 +0700

    Update UI

--- component/App.js
+++ component/App.js
@@ -18,36 +18,83 @@ function App({ location }) {
         <i class="fa-solid fa-magnifying-glass"></i>
       </button>
     </div>
-    <div class="weather">
-      <img
-        class="weather-icon"
-        src="./icons/${location
-          .map((x) => x.result.weather.map((y) => y.icon))
-          .join(" ")
-          .slice(0, -1)}.png"
-      />
-      <div class="info">
-        <h2 class="city">${location.map((x) => x.result.name).join(" ")}</h2>
+    <div class="weather-container">
+      <h1 class="city">${location.map((x) => x.result.name).join(" ")}</h1>
+      <h3 class="description">
+        ${location
+          .map((x) => x.result.weather.map((y) => y.description))
+          .join(" ")}
+      </h3>
+      <div class="weather">
         <h1 class="temp">
-          ${location.map((x) => Math.round(x.result.main.temp))}°C
+          ${location.map((x) => Math.round(x.result.main.temp))}
+          <span class="unit-c">°</span>
         </h1>
-        <h3 class="description">
-          ${location
-            .map((x) => x.result.weather.map((y) => y.description))
-            .join(" ")}
-        </h3>
-        <div class="details">
-          <div class="col">
-            <img src="icons/humidity.png" />
-            <p class="humdity">
-              ${location.map((x) => x.result.main.humidity)}%
-            </p>
-            <p>Humidity</p>
+        <img
+          class="weather-icon"
+          src="./icons/${location
+            .map((x) => x.result.weather.map((y) => y.icon))
+            .join(" ")
+            .slice(0, -1)}.png"
+        />
+      </div>
+      <div class="details">
+        <div class="col">
+          <div class="details_container">
+            <h4 class="title">
+              <i class="fa-solid fa-water"></i>
+              Humidity
+            </h4>
+            <div class="info">
+              <p class="humidity">
+                ${location.map((x) => x.result.main.humidity)}%
+              </p>
+              <p class="message">The dew point right now</p>
+            </div>
           </div>
-          <div class="col">
-            <img src="icons/wind.png" />
-            <p class="wind">${location.map((x) => x.result.wind.speed)}m/s</p>
-            <p>Wind</p>
+        </div>
+
+        <div class="col">
+          <div class="details_container">
+            <h4 class="title">
+              <i class="fa-solid fa-wind"></i>
+              Wind
+            </h4>
+
+            <div class="info">
+              <p class="wind">
+                ${location.map((x) => x.result.wind.speed)} m/s
+              </p>
+              <p class="message">Current wind speed</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="details_container">
+            <h4 class="title">
+              <i class="fa-solid fa-cloud"></i>
+              Cloudiness
+            </h4>
+            <div class="info">
+              <p class="cloud">${location.map((x) => x.result.clouds.all)}%</p>
+              <p class="message">Cloud cover in the sky</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="details_container">
+            <h4 class="title">
+              <i class="fa-solid fa-eye"></i>
+              Visibility
+            </h4>
+            <div class="info">
+              <p class="visibility">
+                ${location.map((x) => x.result.visibility) * 0.001} km
+              </p>
+              <p class="message">It's perfectly clear right now</p>
+            </div>
           </div>
         </div>
       </div>
